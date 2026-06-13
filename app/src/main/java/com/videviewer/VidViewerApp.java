21190a794cf41340545517d8ff157592de27005e
sadiph858-spec <sadiph858@gmail.com>
2026-06-13 13:58:30 +0600

    v3.0.0: Updated VidViewerApp

--- app/src/main/java/com/videviewer/VidViewerApp.java
+++ app/src/main/java/com/videviewer/VidViewerApp.java
@@ -1,16 +1,29 @@
 package com.videviewer;
 
-import android.app.Application;
-import android.content.SharedPreferences;
-import androidx.appcompat.app.AppCompatDelegate;
-import com.videviewer.utils.AppConstants;
+  import android.app.Application;
+  import android.content.Context;
+  import android.content.SharedPreferences;
+  import com.google.android.gms.ads.MobileAds;
+  import com.videviewer.utils.AppConstants;
+  import com.videviewer.utils.LocaleHelper;
+  import com.videviewer.utils.ThemeHelper;
 
-public class VidViewerApp extends Application {
+  public class VidViewerApp extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        // Force dark theme always for this app's design
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-    }
-}
+      private static VidViewerApp instance;
+
+      @Override
+      public void onCreate() {
+          super.onCreate();
+          instance = this;
+          ThemeHelper.applyTheme(this);
+          MobileAds.initialize(this, initializationStatus -> {});
+      }
+
+      @Override
+      protected void attachBaseContext(Context base) {
+          super.attachBaseContext(LocaleHelper.setLocale(base));
+      }
+
+      public static VidViewerApp getInstance() { return instance; }
+  }
