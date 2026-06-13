5edfe018b14ea9a2987484c1ba8127a6559836c9
sadiph858-spec <sadiph858@gmail.com>
2026-06-13 13:57:40 +0600

    v3.0.0: MediaStoreHelper with fixed MediaStore loading

---
+++ app/src/main/java/com/videviewer/utils/MediaStoreHelper.java
@@ -0,0 +1,65 @@
+package com.videviewer.utils;
+
+  import android.content.Context;
+  import android.database.Cursor;
+  import android.net.Uri;
+  import android.provider.MediaStore;
+  import com.videviewer.models.VideoItem;
+  import java.util.ArrayList;
+  import java.util.List;
+
+  public class MediaStoreHelper {
+
+      public static List<VideoItem> getAllVideos(Context context) {
+          List<VideoItem> videos = new ArrayList<>();
+          Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+          String[] projection = {
+              MediaStore.Video.Media._ID,
+              MediaStore.Video.Media.TITLE,
+              MediaStore.Video.Media.DATA,
+              MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
+              MediaStore.Video.Media.DURATION,
+              MediaStore.Video.Media.SIZE,
+              MediaStore.Video.Media.MIME_TYPE,
+              MediaStore.Video.Media.DATE_ADDED
+          };
+          String sortOrder = MediaStore.Video.Media.DATE_ADDED + " DESC";
+
+          try (Cursor cursor = context.getContentResolver().query(uri, projection, null, null, sortOrder)) {
+              if (cursor != null && cursor.moveToFirst()) {
+                  int idCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+                  int titleCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.TITLE);
+                  int dataCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                  int folderCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
+                  int durCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
+                  int sizeCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE);
+                  int mimeCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE);
+                  int dateCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED);
+
+                  do {
+                      VideoItem item = new VideoItem();
+                      item.id = cursor.getLong(idCol);
+                      item.title = cursor.getString(titleCol);
+                      item.path = cursor.getString(dataCol);
+                      item.folder = cursor.getString(folderCol);
+                      item.duration = cursor.getLong(durCol);
+                      item.size = cursor.getLong(sizeCol);
+                      item.mimeType = cursor.getString(mimeCol);
+                      item.dateAdded = cursor.getLong(dateCol);
+                      if (item.path != null && new java.io.File(item.path).exists()) {
+                          videos.add(item);
+                      }
+                  } while (cursor.moveToNext());
+              }
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+          return videos;
+      }
+
+      public static boolean deleteVideo(Context context, String path) {
+          Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+          int deleted = context.getContentResolver().delete(uri, MediaStore.Video.Media.DATA + "=?", new String[]{path});
+          return deleted > 0;
+      }
+  }
