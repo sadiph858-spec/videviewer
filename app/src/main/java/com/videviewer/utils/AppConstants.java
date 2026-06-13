a617716cb5a705c8cd0636af5d8a8cb310f75ead
sadiph858-spec <sadiph858@gmail.com>
2026-06-13 15:41:53 +0600

    fix: add all missing constants (PREFS_NAME, VAULT_FOLDER, LOCK_*, etc.)

--- app/src/main/java/com/videviewer/utils/AppConstants.java
+++ app/src/main/java/com/videviewer/utils/AppConstants.java
@@ -1,21 +1,47 @@
 package com.videviewer.utils;
 
   public class AppConstants {
+      // Intent extras
       public static final String EXTRA_VIDEO_PATH = "extra_video_path";
       public static final String EXTRA_VIDEO_TITLE = "extra_video_title";
       public static final String EXTRA_VIDEO_ID = "extra_video_id";
+      public static final String EXTRA_FOLDER_PATH = "extra_folder_path";
+
+      // Shared prefs
       public static final String PREF_NAME = "vidviewer_prefs";
+      public static final String PREFS_NAME = "vidviewer_prefs";
       public static final String PREF_THEME = "pref_theme";
       public static final String PREF_LANGUAGE = "pref_language";
+
+      // Vault prefs
       public static final String PREF_VAULT_PIN = "pref_vault_pin";
-      public static final String PREF_VAULT_TYPE = "pref_vault_type"; // pin, pattern, password
+      public static final String PREF_VAULT_TYPE = "pref_vault_type";
+      public static final String PREF_VAULT_LOCK_TYPE = "pref_vault_lock_type";
+      public static final String PREF_VAULT_PASSWORD = "pref_vault_password";
+      public static final String PREF_VAULT_PATTERN = "pref_vault_pattern";
+
+      // Lock types
+      public static final String LOCK_NONE = "none";
+      public static final String LOCK_PIN = "pin";
+      public static final String LOCK_PASSWORD = "password";
+      public static final String LOCK_PATTERN = "pattern";
+
+      // Database
       public static final String DB_NAME = "vidviewer_db";
       public static final int DB_VERSION = 4;
+
+      // Vault
       public static final String DOWNLOAD_DIR = "VidViewer";
       public static final String VAULT_DIR = ".vault";
+      public static final String VAULT_FOLDER = ".vault";
+
+      // Notifications
       public static final int NOTIFICATION_DOWNLOAD_ID = 1001;
       public static final String CHANNEL_DOWNLOAD = "channel_download";
+
+      // AdMob (test IDs)
       public static final String ADMOB_BANNER_ID = "ca-app-pub-3940256099942544/6300978111";
       public static final String ADMOB_INTERSTITIAL_ID = "ca-app-pub-3940256099942544/1033173712";
       public static final String ADMOB_REWARDED_ID = "ca-app-pub-3940256099942544/5224354917";
   }
+  
