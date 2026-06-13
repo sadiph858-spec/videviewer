33eeb4d63c89620e785c043599f89927b1390a38
sadiph858-spec <sadiph858@gmail.com>
2026-06-13 13:57:44 +0600

    v3.0.0: CastOptionsProvider

---
+++ app/src/main/java/com/videviewer/utils/CastOptionsProvider.java
@@ -0,0 +1,18 @@
+package com.videviewer.utils;
+
+  import android.content.Context;
+  import com.google.android.gms.cast.framework.CastOptions;
+  import com.google.android.gms.cast.framework.OptionsProvider;
+  import com.google.android.gms.cast.framework.SessionProvider;
+  import java.util.List;
+
+  public class CastOptionsProvider implements OptionsProvider {
+      @Override
+      public CastOptions getCastOptions(Context context) {
+          return new CastOptions.Builder()
+              .setReceiverApplicationId("CC1AD845")
+              .build();
+      }
+      @Override
+      public List<SessionProvider> getAdditionalSessionProviders(Context context) { return null; }
+  }
