7e1e440a95b657651e5736ace5df73a9fff22c86
sadiph858-spec <sadiph858@gmail.com>
2026-06-26 19:55:26 +0600

    fix: use VideoItem setters instead of direct private field access

--- app/src/main/java/com/videviewer/utils/MediaStoreHelper.java
+++ app/src/main/java/com/videviewer/utils/MediaStoreHelper.java
@@ -1,65 +1,66 @@
 package com.videviewer.utils;
 
-  import android.content.Context;
-  import android.database.Cursor;
-  import android.net.Uri;
-  import android.provider.MediaStore;
-  import com.videviewer.models.VideoItem;
-  import java.util.ArrayList;
-  import java.util.List;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import com.videviewer.models.VideoItem;
+import java.util.ArrayList;
+import java.util.List;
 
-  public class MediaStoreHelper {
+public class MediaStoreHelper {
 
-      public static List<VideoItem> getAllVideos(Context context) {
-          List<VideoItem> videos = new ArrayList<>();
-          Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-          String[] projection = {
-              MediaStore.Video.Media._ID,
-              MediaStore.Video.Media.TITLE,
-              MediaStore.Video.Media.DATA,
-              MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
-              MediaStore.Video.Media.DURATION,
-              MediaStore.Video.Media.SIZE,
-              MediaStore.Video.Media.MIME_TYPE,
-              MediaStore.Video.Media.DATE_ADDED
-          };
-          String sortOrder = MediaStore.Video.Media.DATE_ADDED + " DESC";
+    public static List<VideoItem> getAllVideos(Context context) {
+        List<VideoItem> videos = new ArrayList<>();
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+            MediaStore.Video.Media._ID,
+            MediaStore.Video.Media.TITLE,
+            MediaStore.Video.Media.DATA,
+            MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
+            MediaStore.Video.Media.DURATION,
+            MediaStore.Video.Media.SIZE,
+            MediaStore.Video.Media.MIME_TYPE,
+            MediaStore.Video.Media.DATE_ADDED
+        };
+        String sortOrder = MediaStore.Video.Media.DATE_ADDED + " DESC";
 
-          try (Cursor cursor = context.getContentResolver().query(uri, projection, null, null, sortOrder)) {
-              if (cursor != null && cursor.moveToFirst()) {
-                  int idCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
-                  int titleCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.TITLE);
-                  int dataCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
-                  int folderCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
-                  int durCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
-                  int sizeCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE);
-                  int mimeCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE);
-                  int dateCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED);
+        try (Cursor cursor = context.getContentResolver().query(uri, projection, null, null, sortOrder)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                int idCol     = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+                int titleCol  = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.TITLE);
+                int dataCol   = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+                int folderCol = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.BUCKET_DISPLAY_NAME);
+                int durCol    = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DURATION);
+                int sizeCol   = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE);
+                int mimeCol   = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.MIME_TYPE);
+                int dateCol   = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_ADDED);
+                do {
+                    VideoItem item = new VideoItem();
+                    item.setId(cursor.getLong(idCol));
+                    item.setTitle(cursor.getString(titleCol));
+                    item.setPath(cursor.getString(dataCol));
+                    item.setFolderName(cursor.getString(folderCol));
+                    item.setDuration(cursor.getLong(durCol));
+                    item.setSize(cursor.getLong(sizeCol));
+                    item.setMimeType(cursor.getString(mimeCol));
+                    item.setDateAdded(cursor.getLong(dateCol));
+                    item.setFolderPath(cursor.getString(dataCol));
+                    if (item.getPath() != null && new java.io.File(item.getPath()).exists()) {
+                        videos.add(item);
+                    }
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return videos;
+    }
 
-                  do {
-                      VideoItem item = new VideoItem();
-                      item.id = cursor.getLong(idCol);
-                      item.title = cursor.getString(titleCol);
-                      item.path = cursor.getString(dataCol);
-                      item.folder = cursor.getString(folderCol);
-                      item.duration = cursor.getLong(durCol);
-                      item.size = cursor.getLong(sizeCol);
-                      item.mimeType = cursor.getString(mimeCol);
-                      item.dateAdded = cursor.getLong(dateCol);
-                      if (item.path != null && new java.io.File(item.path).exists()) {
-                          videos.add(item);
-                      }
-                  } while (cursor.moveToNext());
-              }
-          } catch (Exception e) {
-              e.printStackTrace();
-          }
-          return videos;
-      }
-
-      public static boolean deleteVideo(Context context, String path) {
-          Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-          int deleted = context.getContentResolver().delete(uri, MediaStore.Video.Media.DATA + "=?", new String[]{path});
-          return deleted > 0;
-      }
-  }
+    public static boolean deleteVideo(Context context, String path) {
+        Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+        int deleted = context.getContentResolver().delete(
+            uri, MediaStore.Video.Media.DATA + "=?", new String[]{path});
+        return deleted > 0;
+    }
+}
